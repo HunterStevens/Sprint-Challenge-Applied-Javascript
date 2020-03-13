4e535e41f862883fd137edc62112db660b9dfc6a
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-13 11:53:20 -0600

    MVP completed

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,20 +19,20 @@
 // Create a card for each of the articles and add the card to the DOM.
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then(res =>{
-    console.log('Cards Array', res);
-    const articleArr = Object.entries(res.data.articles);
-    return articleArr;
-})
-.then(res =>{
-    console.log('Chained Card', res);
-    for(let i = 0; i < res.length ; i++){
-        const newArticle = res[i];
-        cardPlace.appendChild(createCard(newArticle[1]));  
-    }
+    let cardObject = res.data.articles;
+    let articleArr = Object.values(cardObject);
+    articleArr.forEach(article => {
+        console.log('Article 1', article);
+        article.forEach(article2 => {
+            createCard(article2);
+            console.log('Article 2', article2);
+        })
+    });
 })
 .catch(err =>{
     console.log('ERROR: ', err);
 })
+const cardPlace = document.querySelector('.cards-container');
 
 function createCard(item){
     const cardBod = document.createElement('div'),
@@ -42,7 +42,12 @@ function createCard(item){
             cardImg = document.createElement('img'),
             cardAuthor = document.createElement('span');
 
-    cardBod.classlist.add('card');
+    cardBod.append(cardHead, cardAuthorBod);
+    cardAuthorBod.append(cardImgBod, cardAuthor);
+    cardImgBod.appendChild(cardImg);
+    cardPlace.appendChild(cardBod);
+
+    cardBod.classList.add('card');
     cardHead.classList.add('headline');
     cardAuthorBod.classList.add('author');
     cardImgBod.classList.add('img-container');
@@ -50,8 +55,38 @@ function createCard(item){
     cardHead.textContent = item.headline;
     cardImg.src = item.authorPhoto;
     cardAuthor.textContent = item.authorName;
-    
+
     return cardBod;
 }
 
-const cardPlace = document.querySelector('cards-container');
+// axios.get("https://lambda-times-backend.herokuapp.com/articles")
+//     .then(response => {
+//        let cardObj = response.data.articles;
+//        let infoArr = Object.values(cardObj);
+//        infoArr.forEach(cv => {
+//            cv.forEach(cv2 => {
+//             createCard(cv2);
+//            })
+//        })
+//     })
+// const cardCont = document.querySelector(".cards-container");
+// function createCard(obj){
+//     const card = document.createElement("div");
+//     const headline = document.createElement("div");
+//     const author = document.createElement("div");
+//     const imgCont = document.createElement("div");
+//     const img = document.createElement("img");
+//     const authorSpan = document.createElement("span");
+//     card.append(headline, author);
+//     author.append(imgCont, authorSpan);
+//     imgCont.appendChild(img);
+//     cardCont.appendChild(card);
+//     card.classList.add("card");
+//     headline.classList.add("headline");
+//     author.classList.add("author");
+//     imgCont.classList.add("img-container");
+//     headline.textContent = obj.headline;
+//     img.src = obj.authorPhoto;
+//     authorSpan.textContent = obj.authorName;
+//     return card;
+// }
