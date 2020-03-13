bfe59bde3d06419032713f04cb341ba9a6f33e82
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-13 09:46:33 -0600

    Header Almost completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,21 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(data) {
+    const headBod = document.createElement('div'),
+        headDate  = document.createElement('span'),
+        headTitle  = document.createElement('h1'),
+        headTemp  = document.createElement('span');
+        
+    headBod.classList.add('header');
+    headDate.classList.add('date');
+    headTemp.classList.add('temp');
+
+    headBod.append(headDate, headTitle, headTemp);
+
+    headDate.textContent = 'March 13, 2020';
+    headTitle.textContent = 'Lambda Times';
+    headTemp.textContent = '98';
+}
+
+const headPlace = document.querySelector('.header-container');
